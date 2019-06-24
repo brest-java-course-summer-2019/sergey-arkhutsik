e48266ba7c82709f99718a5df2dc97e1a8a682d1
sergeyarhutik <sergey.arhutik@gmail.com>
2019-06-24 13:23:13 +0300

    version 1.02 pricePerKm depends on distance

--- delivery_cost/src/com/epam/brest2019/courses/Main.java
+++ delivery_cost/src/com/epam/brest2019/courses/Main.java
@@ -8,50 +8,32 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static BigDecimal[] array1;
+    private static BigDecimal pricePerKm;
+
     public static void main(String[] args) {
 
         BigDecimal weight;
         BigDecimal distance;
-
-
-        String PATH_TO_PROPERTIES = "delivery_cost/src/com/epam/brest2019/courses/resources/price_per_km.properties";
-        FileInputStream fileInputStream;
-        Properties prop = new Properties();
-        try {
-            fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
-            prop.load(fileInputStream);
-
-            String less10 = prop.getProperty("less10");
-            String less50 = prop.getProperty("less50");
-            String less100 = prop.getProperty("less100");
-            System.out.println("Properties file:");
-            System.out.println("     less10: " + less10);
-            System.out.println("     less50: " + less50);
-            System.out.println("     less100: " + less100);
-
-        } catch (IOException e) {
-            System.err.println("ОШИБКА: Файл " + PATH_TO_PROPERTIES + " отсуствует!");
-        }
-
         BigDecimal pricePerKg = new BigDecimal("30");
-        BigDecimal pricePerKm = new BigDecimal("40");
-
 
 
         System.out.println("Enter the weight in kilograms or 'q' for quit: ");
-        weight = GetDataScanner();
+        weight = getDataScanner();
         System.out.println("Enter the distance in kilometers or 'q' for quit: ");
-        distance = GetDataScanner();
-
+        distance = getDataScanner();
+        readProperties("delivery_cost/src/resources/price_per_km.properties");
+        pricePerKm = compareDistance(distance);
 
         System.out.println("Value of weight = " + weight);
         System.out.println("Value of distance = " + distance);
+        System.out.println("Cost for 1 km = " + pricePerKm);
 
         BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
         System.out.println("Price = " + price);
     }
 
-    private static BigDecimal GetDataScanner(){
+    private static BigDecimal getDataScanner(){
         BigDecimal inputValue = null;
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
@@ -62,4 +44,37 @@ public class Main {
         }
         return inputValue;
     }
+
+    private static BigDecimal[] readProperties(String PATH_TO_PROPERTIES){
+        array1 = new BigDecimal[3];
+        FileInputStream fileInputStream;
+        Properties prop = new Properties();
+        try {
+            fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
+            prop.load(fileInputStream);
+            String less10 = prop.getProperty("less10");
+            String less50 = prop.getProperty("less50");
+            String less100 = prop.getProperty("less100");
+            array1[0] = new BigDecimal(less10);
+            array1[1] = new BigDecimal(less50);
+            array1[2] = new BigDecimal(less100);
+        } catch (IOException e) {
+            System.err.println("ОШИБКА: Файл " + PATH_TO_PROPERTIES + " отсуствует!");
+        }
+        return array1;
+    }
+
+    private static BigDecimal compareDistance(BigDecimal distance){
+        if (distance.compareTo(BigDecimal.valueOf(10)) <= 0){
+            return array1[0];
+        } else  if(distance.compareTo(BigDecimal.valueOf(50)) <= 0){
+            return array1[1];
+        } else  if(distance.compareTo(BigDecimal.valueOf(100)) <= 0) {
+            return array1[2];
+        } else {
+            return BigDecimal.valueOf(50);
+        }
+
+    }
+
 }
