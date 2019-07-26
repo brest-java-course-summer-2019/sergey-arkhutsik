a49b52fc12115f9834e0c2951ce30f65100882fd
sergeyarhutik <sergey.arhutik@gmail.com>
2019-07-26 09:41:21 +0300

    add logback dependencies

--- repair_service/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/controllers/DeviceController.java
+++ repair_service/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/controllers/DeviceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class DeviceController {
     @GetMapping(value = "/devices")
-    public final String departments(Model model) {
+    public final String getDevices(Model model) {
         return "devices";
     }
 }
