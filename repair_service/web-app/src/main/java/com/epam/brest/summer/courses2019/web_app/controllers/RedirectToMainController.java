4d4bef3d5360e749d6e7b18df5759fbf288d690e
sergeyarhutik <sergey.arhutik@gmail.com>
2019-07-29 11:16:28 +0300

    structure adjustment of pom.xml files

--- repair_service/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/controllers/RedirectToMainController.java
+++ repair_service/web-app/src/main/java/com/epam/brest/summer/courses2019/web_app/controllers/RedirectToMainController.java
@@ -2,7 +2,6 @@ package com.epam.brest.summer.courses2019.web_app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Redirect MVC controller.
