f279afb1c9edcbb08e21837c1e87017e0b019271
sergeyarhutik <sergey.arhutik@gmail.com>
2019-09-17 14:32:15 +0300

    add sql attribute Date

--- repair_service/test-db/src/main/resources/data-script.sql
+++ repair_service/test-db/src/main/resources/data-script.sql
@@ -2,8 +2,8 @@ INSERT INTO client (client_id, client_name) VALUES (1, 'Иванов Иван И
 INSERT INTO client (client_id, client_name) VALUES (2, 'Петров Петр Петрович');
 INSERT INTO client (client_id, client_name) VALUES (3, 'Сидоров Петр Иванович');
 
-INSERT INTO device (device_id, device_name, parent_id, device_description) VALUES (1, 'Ipad Air1', 1, 'damaged by water');
-INSERT INTO device (device_id, device_name, parent_id, device_description) VALUES (2, 'Ipad Air2', 1, 'broken display');
-INSERT INTO device (device_id, device_name, parent_id, device_description) VALUES (3, 'Ipad Air3', 2, 'not charging');
-INSERT INTO device (device_id, device_name, parent_id, device_description) VALUES (4, 'Ipad Air4', 2, 'crushed by tank');
-INSERT INTO device (device_id, device_name, parent_id, device_description) VALUES (5, 'Ipad Air5', 2, 'battery runs out quickly');
+INSERT INTO device (device_id, device_name, parent_id, device_date, device_description) VALUES (1, 'Ipad Air1', 1, '2019-07-12 12:00:00', 'damaged by water');
+INSERT INTO device (device_id, device_name, parent_id, device_date, device_description) VALUES (2, 'Ipad Air2', 1, '2019-07-13 12:00:00', 'broken display');
+INSERT INTO device (device_id, device_name, parent_id, device_date, device_description) VALUES (3, 'Ipad Air3', 2, '2019-07-14 12:00:00', 'not charging');
+INSERT INTO device (device_id, device_name, parent_id, device_date, device_description) VALUES (4, 'Ipad Air4', 2, '2019-07-15 12:00:00', 'crushed by tank');
+INSERT INTO device (device_id, device_name, parent_id, device_date, device_description) VALUES (5, 'Ipad Air5', 2, '2019-07-16 12:00:00', 'battery runs out quickly');
