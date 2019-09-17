8b3f64fc7917f3ff747a200b543586598a1b969c
sergeyarhutik <sergey.arhutik@gmail.com>
2019-09-17 15:01:59 +0300

    wep app edit

--- repair_service/dao/src/test/java/com.epam.brest.summer.courses2019.dao/DeviceDaoJdbcImplTest.java
+++ repair_service/dao/src/test/java/com.epam.brest.summer.courses2019.dao/DeviceDaoJdbcImplTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -27,36 +31,43 @@ public class DeviceDaoJdbcImplTest {
     private static final int NEW_PARENT_ID = 2;
     private static final String DEVICE_DESCRIPTION = "Something happened";
     private static final String NEW_DEVICE_DESCRIPTION = "New problem";
+    private SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
 
     @Autowired
     private DeviceDao deviceDao;
 
     @Test
-    public void testAddDevice() {
+    public void testAddDevice() throws ParseException {
+        Date date = formatDate.parse(DATE);
         List<Device> devices = deviceDao.findAll();
         int sizeBefore = devices.size();
-        Device device = new Device(DEVICE_NAME, PARENT_ID, DEVICE_DESCRIPTION);
+        Device device = new Device(DEVICE_NAME, PARENT_ID, date, DEVICE_DESCRIPTION);
         Device addedDevice = deviceDao.addDevice(device);
+        assertTrue(device.getDeviceDate().equals(addedDevice.getDeviceDate()));
         assertTrue((sizeBefore + 1) == deviceDao.findAll().size());
     }
 
     @Test
-    public void testUpdateDevice() {
+    public void testUpdateDevice() throws ParseException {
+        Date newDate = formatDate.parse(NEW_DATE);
         Device newDevice = deviceDao.findById(1).get();
         newDevice.setDeviceName(NEW_DEVICE_NAME);
+        newDevice.setDeviceDate(newDate);
         newDevice.setDeviceDescription(NEW_DEVICE_DESCRIPTION);
         newDevice.setParentId(NEW_PARENT_ID);
         deviceDao.update(newDevice);
         Device updatedDevice = deviceDao.findById(newDevice.getDeviceId()).get();
         assertEquals(updatedDevice.getDeviceId(), newDevice.getDeviceId());
+        assertTrue(newDevice.getDeviceDate().equals(updatedDevice.getDeviceDate()));
         assertEquals(updatedDevice.getDeviceName(), newDevice.getDeviceName());
         assertEquals(updatedDevice.getParentId(), newDevice.getParentId());
     }
 
     @Test
-    public void testDeleteDevice() {
+    public void testDeleteDevice() throws ParseException {
+        Date date = formatDate.parse(DATE);
         Device device = deviceDao.addDevice(
-                new Device(DEVICE_NAME, PARENT_ID, DEVICE_DESCRIPTION));
+                new Device(DEVICE_NAME, PARENT_ID, date, DEVICE_DESCRIPTION));
         int deletedDeviceId = device.getDeviceId();
         deviceDao.delete(deletedDeviceId);
         assertFalse(deviceDao.findById(deletedDeviceId).isPresent());
@@ -69,7 +80,8 @@ public class DeviceDaoJdbcImplTest {
     }
 
     @Test
-    public void testFindDeviceById() {
+    public void testFindDeviceById() throws ParseException {
+        Date date = formatDate.parse(DATE);
         Device testDevice = deviceDao.findById(FIRST_DEVICE_ID).get();
         assertNotNull(deviceDao);
         assertTrue(testDevice.getDeviceId().equals(FIRST_DEVICE_ID));
@@ -78,4 +90,34 @@ public class DeviceDaoJdbcImplTest {
         assertTrue(testDevice.getParentId().equals(1));
     }
 
+    @Test
+    public void testFilterDeviceByDates() throws ParseException {
+        Date fromDate = formatDate.parse(FROM_DATE);
+        Date toDate = formatDate.parse(TO_DATE);
+        List<Device> devices = deviceDao.filterDeviceByDate(fromDate, toDate);
+        assertNotNull(devices);
+        assertTrue(devices.size() > 1);
+    }
+
+    @Test
+    public void testFilterDeviceByNullDates() {
+        List<Device> devicesByDateNull
+                = deviceDao.filterDeviceByDate(null, null);
+        List<Device> allDevices = deviceDao.findAll();
+        assertNotNull(devicesByDateNull);
+        assertNotNull(allDevices);
+        assertTrue(devicesByDateNull.size() == allDevices.size());
+    }
+
+    @Test
+    public void testFilterDeviceByOneNullDate() throws ParseException {
+        Date fromDate = formatDate.parse(FROM_DATE);
+        Date toDate = formatDate.parse(TO_DATE);
+        List<Device> devicesByFromDateNull
+                = deviceDao.filterDeviceByDate(null, toDate);
+        List<Device> devicesByToDateNull
+                = deviceDao.filterDeviceByDate(fromDate, null);
+        assertNotNull(devicesByFromDateNull);
+        assertNotNull(devicesByToDateNull);
+    }
 }
